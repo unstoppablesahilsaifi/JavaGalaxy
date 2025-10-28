4d06ba2b54c041887fa68425536dde67980bf921
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-10-28 20:59:43 +0530

    Code Commit

---
+++ src/Java8/MethodReference.java
@@ -0,0 +1,17 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+// A method reference is shorthand for a lambda expression to call a method directly using ::
+//Syntax: ClassName::methodName
+public class MethodReference {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Sahil", "Amit", "Rohan", "Priya");
+
+        // Lambda Expression
+        names.forEach(name -> System.out.println(name));
+    }
+
+
+}
