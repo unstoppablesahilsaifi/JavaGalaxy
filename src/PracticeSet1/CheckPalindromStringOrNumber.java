b2045f1552366b729ad56c024fbb01f5191aa688
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-10-28 19:55:15 +0530

    Code Commit

--- src/PracticeSet1/CheckPalindromStringOrNumber.java
+++ src/PracticeSet1/CheckPalindromStringOrNumber.java
@@ -1,4 +1,33 @@
 package PracticeSet1;
 
 public class CheckPalindromStringOrNumber {
+    public static void main(String[] args) {
+        // Check Number
+        int n=121;
+        int ans=0;
+        int temp=n;
+        while(temp>0){
+            int rem=temp%10;
+            ans=ans*10+rem;
+            temp=temp/10;
+        }
+        if(ans==n){
+            System.out.println("Yes Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
+
+        //Check String
+        String str="pop";
+        //To Modify as we can not modify String directly
+        StringBuilder reversedString=new StringBuilder();
+        for(int i=str.length()-1;i>=0;i--){
+            reversedString.append(str.charAt(i));
+        }
+        if(str.equals(reversedString.toString())){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
 }
