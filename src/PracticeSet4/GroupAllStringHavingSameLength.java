c5bbff5994ed6f334d40e4654729b495733b747c
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-11-01 12:53:02 +0530

    Code Commit

---
+++ src/PracticeSet4/GroupAllStringHavingSameLength.java
@@ -0,0 +1,12 @@
+package PracticeSet4;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupAllStringHavingSameLength {
+    public static void main(String[] args) {
+        //Input: ["abc","de","fgh","ij"] → {3=[abc, fgh], 2=[de, ij]}
+        List<String> str= Arrays.asList("abc","de","fgh","ij");//
+
+    }
+}
