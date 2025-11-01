9d43953baa8f8a66fd704a30f2ce75562d3cb5fc
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-11-01 13:31:20 +0530

    Code Commit

--- src/PracticeSet4/GroupAllStringHavingSameLength.java
+++ src/PracticeSet4/GroupAllStringHavingSameLength.java
@@ -1,12 +1,30 @@
 package PracticeSet4;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class GroupAllStringHavingSameLength {
     public static void main(String[] args) {
-        //Input: ["abc","de","fgh","ij"] → {3=[abc, fgh], 2=[de, ij]}
-        List<String> str= Arrays.asList("abc","de","fgh","ij");//
+        // Input: ["abc","de","fgh","ij"] → {3=[abc, fgh], 2=[de, ij]}
+        List<String> str = Arrays.asList("abc", "de", "fgh", "ij");
+        Map<Integer, List<String>> ans = new HashMap<>();
 
+        // simple for loop
+        for (int i = 0; i < str.size(); i++) {
+            String s = str.get(i);
+            int len = s.length();
+
+            // agar map me length pehle se hai
+            if (ans.containsKey(len)) {
+                ans.get(len).add(s); // existing list me add karo
+            }
+            else {
+                // nahi hai to new list banao aur map me daal do
+                List<String> temp = new ArrayList<>();
+                temp.add(s);
+                ans.put(len, temp);
+            }
+        }
+
+        System.out.println("ans = " + ans);
     }
 }
