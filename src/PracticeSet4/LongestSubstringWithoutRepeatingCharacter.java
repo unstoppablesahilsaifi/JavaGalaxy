8605858c1a95be5137684adce2c9d001681cd947
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2025-11-05 17:08:00 +0530

    Code Commit

--- src/PracticeSet4/LongestSubstringWithoutRepeatingCharacter.java
+++ src/PracticeSet4/LongestSubstringWithoutRepeatingCharacter.java
@@ -1,11 +1,38 @@
 package PracticeSet4;
 
+import java.util.HashSet;
+
 public class LongestSubstringWithoutRepeatingCharacter {
     public static void main(String[] args) {
-        //"abcabcbb"
-        //"abc"
+        String str = "abcabcbb";
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        String longest = "";
 
-        String str="abcabcbb";
+        HashSet<Character> set = new HashSet<>();
 
+        while (right < str.length()) {
+            char current = str.charAt(right);
+
+            // Agar character already set me hai, to left se remove karo
+            while (set.contains(current)) {
+                set.remove(str.charAt(left));
+                left++;
+            }
+
+            // Add current character and move right
+            set.add(current);
+            right++;
+
+            // Max length update
+            if (right - left > maxLength) {
+                maxLength = right - left;
+                longest = str.substring(left, right);
+            }
+        }
+
+        System.out.println("Longest substring = " + longest);
+        System.out.println("Length = " + maxLength);
     }
 }
