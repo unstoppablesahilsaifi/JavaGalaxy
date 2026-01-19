f79d5af3510de8b78d93d3ec63fc07707bd4c1a0
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-01-19 19:10:53 +0530

    Code Commit

--- src/MultiThreadingMastery/Step8_Synchronization_FixedRaceCondition.java
+++ src/MultiThreadingMastery/Step8_Synchronization_FixedRaceCondition.java
@@ -1,4 +1,32 @@
 package MultiThreadingMastery;
+class CounterForSync{
+    int count=0;
+   synchronized   void increment(){
+        count++;
+    }
+}
+class MyThreadForSync extends Thread{
+    CounterForSync counter;
+    MyThreadForSync(CounterForSync counter){
+        this.counter=counter;
+    }
 
+    @Override
+    public void run() {
+        for(int i=0;i<500;i++){
+            counter.increment();
+        }
+    }
+}
 public class Step8_Synchronization_FixedRaceCondition {
+    public static void main(String[] args) throws InterruptedException {
+        CounterForSync counter=new CounterForSync();
+        MyThreadForSync th1=new MyThreadForSync(counter);
+        MyThreadForSync th2=new MyThreadForSync(counter);
+        th1.start();
+        th2.start();
+        th1.join();
+        th2.join();
+        System.out.println("Final Counter :   "+ counter.count);
+    }
 }
