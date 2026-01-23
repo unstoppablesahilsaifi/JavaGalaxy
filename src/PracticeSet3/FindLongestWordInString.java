c556b5285a8ff725618b7c4ab135a6798757098c
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-01-23 14:33:23 +0530

    Code Commit

--- src/PracticeSet3/FindLongestWordInString.java
+++ src/PracticeSet3/FindLongestWordInString.java
@@ -2,12 +2,13 @@ package PracticeSet3;
 
 public class FindLongestWordInString {
     public static void main(String[] args) {
-        String str="I am sahil saifi and I am Java Developer";
+        String str="I am sahil saifi and I am Java Developer ok";
         String[] arr=str.split(" ");
-        String temp=arr[0];
+        int maxlen=0;
         String ans="";
         for(int  i=0;i<arr.length;i++){
-            if(temp.length()<arr[i].length()){
+            if(maxlen <arr[i].length()){
+                maxlen=arr[i].length();
                 ans=arr[i];
             }
         }
