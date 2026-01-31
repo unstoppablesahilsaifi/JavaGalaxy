c1430aff9bda983ceb0673e70abad040f3bfb84c
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-01-31 15:23:51 +0530

    Code Commit

---
+++ src/MultiThreadingMastery/Step11A_DaemonThread.java
@@ -0,0 +1,8 @@
+package MultiThreadingMastery;
+//Daemon threads are low-priority background threads that automatically stop when all user threads finish.
+//JVM sirf USER threads ka wait karti hai isliye main thread khatam to daemon bhi khatam
+
+
+
+public class Step11A_DaemonThread {
+}
