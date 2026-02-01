4b0f75f271cda6a657bd9adabdff35f62202d058
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-02-01 12:08:37 +0530

    Code Commit

--- src/MultiThreadingMastery/Step11A_DaemonThread.java
+++ src/MultiThreadingMastery/Step11A_DaemonThread.java
@@ -3,6 +3,40 @@ package MultiThreadingMastery;
 //JVM sirf USER threads ka wait karti hai isliye main thread khatam to daemon bhi khatam
 
 
+class DaemonDemo extends Thread {
 
-public class Step11A_DaemonThread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("Daemon thread running...");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+public class Step11A_DaemonThread {
+    public static void main(String[] args) {
+        DaemonDemo daemonThread = new DaemonDemo();
+
+        daemonThread.setDaemon(true); // 🔥 daemon bana diya
+        daemonThread.start();
+
+        System.out.println("Main thread ends");
+    }
+}
+/*1️⃣ Garbage Collector (Sabse best example)
+
+JVM ka Garbage Collector ek daemon thread hota hai
+
+2️⃣ Background Monitoring / Health Check
+
+Example:
+
+CPU usage monitor
+
+Memory monitor
+
+Thread count monitor*/
