af2ec17b7640892735fd70249e036601c0ecb6a8
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-03-15 14:45:26 +0530

    Code Commit

---
+++ src/SOLID_Principles/DIP_5/Bad_Design_DIP.java
@@ -0,0 +1,5 @@
+package SOLID_Principles.DIP_5;
+//Dependency Inversion Principle (DIP)
+
+public class Bad_Design_DIP {
+}
--- src/SOLID_Principles/SRP_1/Bad_Design_SRP.java
+++ src/SOLID_Principles/SRP_1/Bad_Design_SRP.java
@@ -1,4 +1,22 @@
 package SOLID_Principles.SRP_1;
+
+/*1️⃣ Why is it Needed?
+Software is never a one-day project. Over time, it goes through updates, changes, bug fixes, and new features.
+If a class/module starts doing more than one type of job:
+❌ Every small change can cause side effects
+❌ Code becomes hard to understand
+❌ Maintenance time and cost increase
+❌ Testing takes longer because a change in one part can break another part
+SRP solves this problem by saying — a class should have only one responsibility.*/
+
+/*2️⃣ What is SRP?
+📖 Definition (Robert C. Martin):
+“A class should have only one reason to change.”
+💡 Meaning:
+A class should have only one responsibility (or purpose)
+If a class has more than one reason to change, it is violating SRP
+🗣 In simple words: If a person is an accountant, HR, and driver at the same time, mistakes will happen.
+Same logic applies to code — one class should not try to do everything.*/
 class UserManager_SRP{
     public void saveUser(String name){
         System.out.println("User " + name + " has been saved");
