6ab77463bc146118e6e1a94408a865b65e2a898f
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-03-21 21:26:08 +0530

    Code Commit

--- src/Design_Patterns/Creational_Patterns/Singleton_Pattern/Bad_Design_Without_Singleton.java
+++ src/Design_Patterns/Creational_Patterns/Singleton_Pattern/Bad_Design_Without_Singleton.java
@@ -5,6 +5,20 @@ package Design_Patterns.Creational_Patterns.Singleton_Pattern;
 👤 Government PM/President → Sirf ek hota hai poore desh ka. Har jagah se wahi instance access hota hai.
 🖨️ Printer Spooler → Agar multiple objects ban gaye toh sab apna-apna print queue maintain karenge → mess ho jayega. Isliye ek hi instance chahiye jo sab handle kare.
 Database Connection → Ek hi shared connection pool maintain karna efficient hai.*/
+class DatabaseConnection_Without_Singleton{
+    public DatabaseConnection_Without_Singleton(){
+        System.out.println("Connection has been created");
+    }
+}
 
 public class Bad_Design_Without_Singleton {
+    public static void main(String[] args) {
+        DatabaseConnection_Without_Singleton db1=new DatabaseConnection_Without_Singleton();
+        DatabaseConnection_Without_Singleton db2=new DatabaseConnection_Without_Singleton();
+        System.out.println(db1);
+        System.out.println(db2);
+    }
 }
+/*❌ Issue:
+Har new DatabaseConnection() call ek naya object banata hai.
+Database connections heavy hote hain → multiple connections banenge toh memory waste hoga.*/
