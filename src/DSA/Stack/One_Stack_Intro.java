f5b582a0a61444b6f46404d274ee69937149ed8a
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-08-01 23:46:44 +0530

    Code Commit

---
+++ src/DSA/Stack/One_Stack_Intro.java
@@ -0,0 +1,145 @@
+package DSA.Stack;
+
+/*
+1. What is Stack?
+Definition:
+A Stack is a linear data structure that follows the LIFO (Last In First Out) principle.
+
+Simple language me:
+Jo element sabse last me andar jayega, wahi sabse pehle bahar niklega.
+
+
+Real Life Example 2: Browser Back Button
+Google
+
+↓
+
+YouTube
+
+↓
+
+Instagram
+
+↓
+
+Facebook
+
+Back dabaya
+
+Facebook remove
+
+Fir Back
+
+Instagram remove
+
+Fir
+
+YouTube
+
+Exactly Stack.
+
+Real Life Example 3: Undo (Ctrl + Z)
+Typing
+
+↓
+
+A
+
+↓
+
+B
+
+↓
+
+C
+
+↓
+
+D
+
+Undo
+
+D remove
+
+Undo
+
+C remove
+
+Undo
+
+B remove
+
+Again Stack.
+
+
+2. Why do we need Stack?
+
+Question:
+
+Array bhi data store karta hai.
+
+Linked List bhi data store karti hai.
+
+To Stack ki zarurat hi kya hai?
+
+Answer:
+
+Stack storage ke liye nahi, restricted access dene ke liye banaya gaya hai.
+
+Array me
+
+10 20 30 40 50
+
+Tum kisi bhi element ko access kar sakte ho.
+
+arr[2]
+
+arr[4]
+
+arr[0]
+
+Lekin Stack me nahi.
+
+Sirf
+
+Top
+
+accessible hota hai.
+
+Example
+
+40
+
+30
+
+20
+
+10
+
+Tum
+
+20
+
+directly nahi nikal sakte.
+
+Pehle
+
+40
+
+niklega.
+
+Fir
+
+30
+
+Fir
+
+20
+
+Ye restriction hi Stack ko useful banati hai.
+
+
+
+* */
+public class One_Stack_Intro {
+}
