201480f2288a48e213945a44428aae3511880be2
unstoppablesahilsaifi <sahilsaifi4962@gmail.com>
2026-08-02 20:00:58 +0530

    Code Commit

--- src/DSA/Stack/Part5_Balanced_Parentheses.java
+++ src/DSA/Stack/Part5_Balanced_Parentheses.java
@@ -15,5 +15,47 @@ Output
 
 Not Balanced
 */
+
+import java.util.Stack;
+
 public class Part5_Balanced_Parentheses {
+
+    public static boolean isBalanced(String str){
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+
+            char ch = str.charAt(i);
+
+            if(ch=='('){
+
+                stack.push(ch);
+
+            }else{
+
+                if(stack.isEmpty()){
+
+                    return false;
+
+                }
+
+                stack.pop();
+
+            }
+
+        }
+
+        return stack.isEmpty();
+
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(isBalanced("(())"));
+
+        System.out.println(isBalanced("(()))"));
+
+    }
+
 }
